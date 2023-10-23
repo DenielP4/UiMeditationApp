a7abf424c63bc0ea350a8565ad0aef3fc4b3481c
Popov Daniil <70586166+DenielP4@users.noreply.github.com>
2023-10-24 00:22:53 +0300

    Upper Ui

---
+++ app/src/main/java/com/example/uimeditationapp/Feature.kt
@@ -0,0 +1,11 @@
+package com.example.uimeditationapp
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class Feature(
+    val title: String,
+    @DrawableRes val iconId: Int,
+    val mediumColor: Color,
+    val darkColor: Color
+)
---
+++ app/src/main/java/com/example/uimeditationapp/ui/HomeScreen.kt
@@ -0,0 +1,251 @@
+package com.example.uimeditationapp.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.uimeditationapp.Feature
+import com.example.uimeditationapp.R
+import com.example.uimeditationapp.standartQuadFromTo
+import com.example.uimeditationapp.ui.theme.ButtonBlue
+import com.example.uimeditationapp.ui.theme.DarkerButtonBlue
+import com.example.uimeditationapp.ui.theme.DeepBlue
+import com.example.uimeditationapp.ui.theme.LightRed
+import com.example.uimeditationapp.ui.theme.TextWhite
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+            ChipSection(
+                chips = listOf(
+                    "Идеально",
+                    "Нормально",
+                    "Грустно",
+                    "Депрессивно",
+                    "Плохо",
+                    "Нервно"
+                )
+            )
+            CurrentMeditation()
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Daniil"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Доброе утро, $name",
+                style = MaterialTheme.typography.h2
+
+            )
+            Text(
+                text = "Желаю Вам хорошего дня!",
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, end = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ) {
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+) {
+
+    var selectedPlayItem by remember {
+        mutableStateOf(false)
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 15.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text(
+                text = "Плейлист Дня",
+                style = MaterialTheme.typography.h2
+
+            )
+            Text(
+                text = "Медитация 3-10 минут",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+                .clickable {
+                    selectedPlayItem = !selectedPlayItem
+                }
+        ) {
+            Icon(
+                painter =
+                if (selectedPlayItem)
+                    painterResource(id = R.drawable.ic_pause)
+                else
+                    painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun FeatureSection(
+    features: List<Feature>
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(features.size) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standartQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standartQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standartQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standartQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+    }
+}
